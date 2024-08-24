17bfd8dbf0078170819e91df1cf13eb924853919
Hamunii <samuel.suni04@gmail.com>
2024-08-24 04:02:15 +0300

    feat: Validate method & ModDefinition from reflection

--- src/ContentLib.Core/ContentDefinition.cs
+++ src/ContentLib.Core/ContentDefinition.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using BepInEx;
 using UnityEngine;
 
 namespace ContentLib.Core;
@@ -25,18 +30,34 @@ public abstract class ContentDefinition : ScriptableObject
     public bool IsRegistered { get; protected set; }
 
     /// <summary>
+    /// Validates this content.
+    /// </summary>
+    /// <returns>If this content passed validation, or otherwise a message with each issue.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)] // Aggressive inlining is for GetCallingAssembly!
+    public virtual (bool isValid, string? message) Validate()
+    {
+        (bool isValid, string? message) result = new();
+
+        if (!TryGetModDefinitionFromCallingAssembly(out ModDefinition? modDefinition))
+        {
+            MarkAsInvalid(ref result,
+                $"Tried getting {nameof(BepInPlugin)} Attribute from CallingAssembly, but the attribute was not found!\n" +
+                "Make sure to call ContentLib from a valid BepInEx Plugin assembly.");
+        }
+
+        Mod = modDefinition;
+
+        return result;
+    }
+
+    /// <summary>
     /// Validates and registers this content.
     /// </summary>
     /// <remarks>
     /// If this method completes without throwing, <see cref="IsRegistered"/> of this instance
     /// is set to <see langword="true"/>.
     /// </remarks>
-    public virtual void Register(ModDefinition modDefinition)
-    {
-        // We don't need to assign Mod to modDefinition.GetRealInstance because the
-        // *Definition that inherits this class already passed us the real instance.
-        Mod = modDefinition;
-    }
+    public abstract void Register(ModDefinition modDefinition);
 
     /// <summary>
     /// Warn according to <see cref="WarningSeverity"/>.
@@ -62,5 +83,49 @@ public abstract class ContentDefinition : ScriptableObject
             default:
                 throw new InvalidOperationException($"{nameof(WarningSeverity)} isn't set to a valid value! Value: {WarningSeverity}");
         }
+    }
+
+    /// <summary>
+    /// Used for marking a <see cref="Validate"/> return value as invalid.
+    /// </summary>
+    /// <param name="result">The object the <see cref="Validate"/> method will return.</param>
+    /// <param name="message">A message that explains why the <see cref="ContentDefinition"/> was marked as invalid.</param>
+    protected void MarkAsInvalid(ref (bool isValid, string? message) result, string message)
+    {
+        result.isValid = false;
+        result.message += message;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)] // Aggressive inlining is for GetCallingAssembly!
+    private bool TryGetModDefinitionFromCallingAssembly([NotNullWhen(returnValue: true)] out ModDefinition? modDefinition)
+    {
+        modDefinition = null;
+        var callingAssembly = Assembly.GetCallingAssembly();
+
+        Type[] types;
+        try
+        {
+			types = callingAssembly.GetTypes();
+		}
+        catch(ReflectionTypeLoadException ex)
+        {
+			types = ex.Types.Where(t => t != null).ToArray();
+		}
+
+        foreach (Type type in types)
+        {
+            if (type.IsSubclassOf(typeof(BaseUnityPlugin)))
+            {
+                var bepInPluginAttribute = (BepInPlugin)Attribute.GetCustomAttribute(type, typeof(BepInPlugin));
+
+                if (bepInPluginAttribute is null)
+                    continue;
+
+                modDefinition = ModDefinition.Create(bepInPluginAttribute);
+                return true;
+            }
+        }
+
+        return false;
     }
 }
