6f0c1d5b9e801bb44e5263a983ec95fd69a2182a
RazielMartelus <razielmartelus@rathnirdevteam.onmicrosoft.com>
2024-09-05 08:40:52 +0100

    LocalBrackenMonsterKIllPlayerEvent has been created and added to BrackenPatches.cs, along with a working method for triggering the event being added to BrackenPatches.FlowerManAI_OnCollideWithPlayer.
    Both Collide and Kill methods are tested within TestListener.cs now.

--- src/ContentLib.EnemyAPI/Test/TestListener.cs
+++ src/ContentLib.EnemyAPI/Test/TestListener.cs
@@ -9,21 +9,27 @@ public class TestListener
 {
     public TestListener()
     {
-        GameEventManager.Instance.Subscribe<MonsterCollideWithPlayerEvent>(GameEventType.MonsterPlayerCollisionEvent,
-            OnMonsterColision);
+        GameEventManager.Instance.Subscribe<MonsterKillsPlayerEvent>(GameEventType.MonsterKillsPlayerEvent,
+            OnMonsterKill);
+        
+        GameEventManager.Instance.Subscribe<MonsterCollideWithPlayerEvent>(GameEventType.MonsterPlayerCollisionEvent,OnMonsterCollision);
         Debug.Log("TestListener has been registered");
     }
-    
-    private void OnMonsterColision(MonsterCollideWithPlayerEvent collideEvent)
+
+
+    private void OnMonsterCollision(MonsterCollideWithPlayerEvent colideEvent) => Debug.Log("Collision Occured");
+
+    private void OnMonsterKill(MonsterKillsPlayerEvent killsPlayerEvent)
     {
-        Debug.Log("OnMonsterColision Check");
-        IEnemy enemy = collideEvent.Enemy;
+        Debug.Log("On Monster Kill Check!");
+        IEnemy enemy = killsPlayerEvent.Enemy;
         Debug.Log($"The Enemy Type: {enemy.EnemyProperties.EnemyClassType}");
         if(enemy is IBracken)
             Debug.Log("It worked and is works");
-        if (EnemyManager.Instance().IsEnemyTypeOf(enemy,typeof(IBracken)))
+        if (enemy is IBracken bracken)
         {
             Debug.Log("[LC-ContentLib] The player has been killed by a Braken!");
+            bracken.AvoidClosestPlayer();
         }
     }
 }
