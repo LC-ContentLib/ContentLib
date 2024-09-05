92aae1cbb6e56bc07d3789d755a25d9575488620
RazielMartelus <razielmartelus@rathnirdevteam.onmicrosoft.com>
2024-09-05 08:54:46 +0100

    -Clean up of TestListener.cs to also show bracken id on kill.

--- src/ContentLib.EnemyAPI/Test/TestListener.cs
+++ src/ContentLib.EnemyAPI/Test/TestListener.cs
@@ -21,15 +21,10 @@ public class TestListener
 
     private void OnMonsterKill(MonsterKillsPlayerEvent killsPlayerEvent)
     {
-        Debug.Log("On Monster Kill Check!");
         IEnemy enemy = killsPlayerEvent.Enemy;
-        Debug.Log($"The Enemy Type: {enemy.EnemyProperties.EnemyClassType}");
-        if(enemy is IBracken)
-            Debug.Log("It worked and is works");
         if (enemy is IBracken bracken)
         {
-            Debug.Log("[LC-ContentLib] The player has been killed by a Braken!");
-            bracken.AvoidClosestPlayer();
+            Debug.Log($"[$LC-ContentLib] The player has been killed by a Braken with id: {bracken.Id}");
         }
     }
 }
