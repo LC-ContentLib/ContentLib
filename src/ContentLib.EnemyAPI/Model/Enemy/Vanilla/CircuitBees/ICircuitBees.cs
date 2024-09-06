cdec2d8186029ce168b2fb86a35f1f66ba146124
RazielMartelus <razielmartelus@rathnirdevteam.onmicrosoft.com>
2024-09-06 19:21:33 +0100

    - Added documentation for ICircuitBees.IsRoaming.

--- src/ContentLib.EnemyAPI/Model/Enemy/Vanilla/CircuitBees/ICircuitBees.cs
+++ src/ContentLib.EnemyAPI/Model/Enemy/Vanilla/CircuitBees/ICircuitBees.cs
@@ -8,5 +8,9 @@ namespace ContentLib.EnemyAPI.Model.Enemy.Vanilla.CircuitBees;
 /// </summary>
 public interface ICircuitBees : IEnemy
 {
+    /// <summary>
+    /// Checkable state for if the bees are currently roaming (i.e. not at their nest due to any reason other than
+    /// chasing a player). 
+    /// </summary>
     bool IsRoaming { get; }
 }
