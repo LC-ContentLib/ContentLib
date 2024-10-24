58c87ff67e5c108ea157da6fa99cb39e4dabf8ed
RazielMartelus96 <razielmartelus@curiosity-core.com>
2024-10-24 18:57:04 +0100

    - Added IForestKeeper.cs, ready for population.

---
+++ src/ContentLib.API/Model/Entity/Enemy/Vanilla/ForestKeeper/IForestKeeper.cs
@@ -0,0 +1,10 @@
+using ContentLib.EnemyAPI.Model.Enemy;
+
+namespace ContentLib.API.Model.Entity.Enemy.Vanilla.ForestKeeper;
+/// <summary>
+/// Interface representing the general functionality of the Forest Keeper Vanilla Enemy.
+/// </summary>
+public interface IForestKeeper: IEnemy
+{
+    
+}
