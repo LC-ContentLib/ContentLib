d4ba923e04986bcd2b1a0ab78cb09119b29b9131
RazielMartelus96 <razielmartelus@curiosity-core.com>
2024-10-24 18:54:03 +0100

    - Added the IEyelessDog.cs interface, ready for population.

---
+++ src/ContentLib.API/Model/Entity/Enemy/Vanilla/EyelessDog/IEyelessDog.cs
@@ -0,0 +1,11 @@
+using ContentLib.EnemyAPI.Model.Enemy;
+
+namespace ContentLib.API.Model.Entity.Enemy.Vanilla.EyelessDog;
+
+/// <summary>
+/// Interface representing the general functionality of the Eyeless Dog Vanilla Enemy. 
+/// </summary>
+public interface IEyelessDog : IEnemy
+{
+    
+}
