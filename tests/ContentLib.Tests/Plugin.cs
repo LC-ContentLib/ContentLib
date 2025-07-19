ea442b2df95191aa435c2d3728bcc18ad37c93c6
Hamunii <samuel.suni04@gmail.com>
2025-07-20 00:19:01 +0300

    add test project

---
+++ tests/ContentLib.Tests/Plugin.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace ContentLib.Tests;
+
+/// <summary>
+/// BepInEx plugin of ContentLib.Items.
+/// Depend on this with <c>[BepInDependency(ItemsPlugin.Id)]</c>.
+/// </summary>
+[BepInAutoPlugin]
+public partial class TestsPlugin : BaseUnityPlugin
+{
+    internal static ManualLogSource Log { get; } = BepInEx.Logging.Logger.CreateLogSource(Name);
+
+    private void Awake()
+    {
+        Log.LogInfo($"Plugin {Name} is loaded!");
+    }
+}
